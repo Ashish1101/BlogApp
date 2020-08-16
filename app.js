448fee24e2c508400be4312fc8c6f6bdb9267779
Ashish <ashishskkumar321@gmail.com>
2020-08-16 19:05:36 +0530

    deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-
+const path = require('path')
 
 //datebase
 const mongoDB = require('./config/db')
@@ -28,6 +28,14 @@ app.use('/forgot', require('./routes/forgot'))
 app.use('/reset', require('./routes/reset'))
 app.use('/blog', require('./routes/blog/blog'))
 
+//server static data (React)
 
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+
+    app.use(express.static('client/build'))
+
+    app.get('*' , (req , res) => res.sendFile(path.resolve(__dirname , 'client', 'build', 'index.html')))
+}
 
 app.listen(port, () => console.log('connected on port' + port))
