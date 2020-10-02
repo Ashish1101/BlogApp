cfb433a91be8c3a8ed2869325de29fb8f2a52ec2
Ashish <ashishskkumar321@gmail.com>
2020-10-02 09:36:10 +0530

    windows path

--- app.js
+++ app.js
@@ -31,12 +31,14 @@ app.use('/blog', require('./routes/blog/blog'))
 //server static data (React)
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname , '/client/build')));
+    app.use(express.static(path.join(__dirname+'\\client\\build')));
 
     app.get('*' , (req, res) => {
         res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'))
     })
 }
+
+console.log(__dirname+'\\client\\build')
 
 
 app.listen(port, () => console.log('connected on port' + port))
